edec03aaef45546891527242a146987508a1f060
Vladislavevteev16 <Vladislavevteev16@users.noreply.github.com>
2026-05-08 21:49:39 +0000

    deploy: 629c6eb341650570453f16856ae9a850cd34aea3

--- assets/react-BufXqxKr.js
+++ assets/react-BufXqxKr.js
@@ -1,4 +1,4 @@
-import{n as e,r as t}from"./index-Cxii6ws2.js";var n=t(e(),1),r=`https://open.spotify.com`,i=/open\.spotify\.com\/(\w+)\/(\w+)/i,a=`https://open.spotify.com/embed-podcast/iframe-api/v1`,o=`SpotifyIframeApi`,s=`onSpotifyIframeApiReady`;function c(e,t={}){return`
+import{n as e,r as t}from"./index-B3E1s0Iw.js";var n=t(e(),1),r=`https://open.spotify.com`,i=/open\.spotify\.com\/(\w+)\/(\w+)/i,a=`https://open.spotify.com/embed-podcast/iframe-api/v1`,o=`SpotifyIframeApi`,s=`onSpotifyIframeApiReady`;function c(e,t={}){return`
     <style>
       :host {
         display: inline-block;
